1bb4c26b6e26a36fde5461bb1b7f17fa20444852
manildayma <manil.dayma.98@gmail.com>
2019-06-10 13:48:05 +0530

    added another proejct

---
+++ Bday Card/Assets/DataFiles/Script/changeMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class changeMenu : MonoBehaviour
+{
+
+    public AudioSource src;
+
+    public void menucard()
+    {
+        SceneManager.LoadScene("MenuScene");
+        
+    }
+
+    public void greetingcard()
+    {
+        SceneManager.LoadScene("GreetingCardScene");
+
+    }
+
+    public void rotatecube()
+    {
+        SceneManager.LoadScene("CubeScene");
+    }
+
+    public void exitapp()
+    {
+        Application.Quit();
+        Debug.Log("Exit Button Pressed.");
+    }
+
+   /*     public void playMusic()
+        {
+            src.Play();
+        }
+        public void pauseMusic()
+        {
+            src.Pause();
+        }
+        */
+}
